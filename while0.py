bd44cdba0a1f0a88f0ca7cb720ac09247cc24d40
Ligiane Basques <ligianealzie25@gmail.com>
2021-10-13 08:25:13 -0400

    while0

--- while0.py
+++ while0.py
@@ -3,10 +3,12 @@
     #print(c)
 #print("FIM")
 
+
+# quando a gente sabe o limite e também não sabe, usa-se o "while".
+# quando a gente sabe o limite usa-se o "for"
 c = 1
 while c < 10:
     print(c)
     c = c + 1
-
 print('Fim')
 
