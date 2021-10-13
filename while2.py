1a7a3877251b21b5ac6d901bee46cacaada69c20
Ligiane Basques <ligianealzie25@gmail.com>
2021-10-13 10:18:03 -0400

    while2

--- while2.py
+++ while2.py
@@ -1,5 +1,10 @@
 
 n = 1
+par = impar = 0
 while n != 0:
     n = int(input('Digite um valor: '))
-print('Acabou')
+    if n % 2 == 0:
+        par = par + 1
+    else:
+        impar = impar + 1
+print('VocÊ digitou {} números pares e {} números ímpares!'.format(par, impar))
