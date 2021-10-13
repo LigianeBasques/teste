6d3cd8a4c21da41c3fb28edc1944415b7454cfcf
Ligiane Basques <ligianealzie25@gmail.com>
2021-10-13 09:22:02 -0400

    while1

--- while1.py
+++ while1.py
@@ -3,7 +3,13 @@
     #n = int(input("Digite um valor"))
 #print("fim")
 
-n = 1
-while n != 0:
+#n = 1
+#while n != 0:
+    #n = int(input('Digite um valor: '))
+#print("FIM")
+
+r = 'S'
+while r == 'S':
     n = int(input('Digite um valor: '))
-print("FIM")
+    r = str(input('Quer continuar? [S/N]')).upper()
+print('FIM')
