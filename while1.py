ca7bcc8131273c614b9388f0eddbb276650badcb
Ligiane Basques <ligianealzie25@gmail.com>
2021-10-13 08:50:00 -0400

    while1

--- while1.py
+++ while1.py
@@ -1,4 +1,9 @@
 
-for c in range(1,5):
-    n = int(input("Digite um valor"))
-print("fim")
+#for c in range(1,5):
+    #n = int(input("Digite um valor"))
+#print("fim")
+
+n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+print("FIM")
