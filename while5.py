d216a35d5a97cf9faa314510fbaa35aec75b3359
Ligiane Basques <ligianealzie25@gmail.com>
2021-10-14 10:28:26 -0400

    while5

--- while5.py
+++ while5.py
@@ -1,9 +1,11 @@
 
 n = s = 0
-while n != 999:
+while True:
     n = int(input('Digite um número: '))
+    if n == 999:
+        break
     s = s + n
-s = s - 999
-print('A soma vale {}'.format(s))
+#print('A soma vale {}'.format(s))
+print( f'A soma vale {s}')
 
 
