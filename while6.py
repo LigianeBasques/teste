67e1df647a38cc0353540f42d58a650134486fed
Ligiane Basques <ligianealzie25@gmail.com>
2021-10-14 14:15:35 -0400

    while6

--- while6.py
+++ while6.py
@@ -1,8 +1,9 @@
 
-soma =0
+soma = cont = 0
 while True:
     num = int(input('Digite um valor (999 para parar): '))
     if num == 999:
         break
+    cont = cont + 1
     soma = soma + num
-print('A soma dos valores foi {}!'.format(soma))
+print(f'A soma dos {cont} valores foi {soma}!')
